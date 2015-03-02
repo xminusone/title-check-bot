7a23c5908353c06ed96dc39b1ad75fb205d67a3a
x_minus_one <xminusone@users.noreply.github.com>
2015-03-02 14:48:30 -0500

    Bot loops correctly, won't remove items with greater than 50 upvotes.

--- TitleCheckBot.py
+++ TitleCheckBot.py
@@ -10,23 +10,24 @@ password = "bot_password"
 
 # Submission Limit (max submissions checked - for testing purposes)
 # Change "1000" to whatever you'd like the limit to be. (1000 is PRAW's max submissions it will pull)
-thing_limit = 100
+thing_limit = 1000
 
 # Startup stuff
-print('Title Check Bot')
+print('Title Check Bot Beta 1 - Starting Up...')
 print('Waiting for Windows to start...') # I have the bot run at boot, if you don't want this extra wait time, comment out the next line.
-time.sleep(5)
+time.sleep(10)
 print('Reticulating splines...')
-time.sleep(3)
+time.sleep(5)
 
 # Reddit login and user agent
-reddit = praw.Reddit(user_agent="Title Check Bot v0.2 (your username here)")
+reddit = praw.Reddit(user_agent="Title Check Bot Beta 1 (your username here)")
 print('Logging in to Reddit...')
 reddit.login(username, password)
 print('Logged in successfully.  Checking for unmoderated items...')
 
 # Subreddit being monitored (you should use "mod" if you want to check all subreddits the bot mods)
 subreddit = reddit.get_subreddit("mod")
+
 
 # Grabs Article Text
 def URLisValid(url):
@@ -42,15 +43,24 @@ def getArticleText(url):
         return str(soup)
 
 # Reddit Stuff
-for submission in subreddit.get_unmoderated(limit=thing_limit):
-    title = submission.title
-    articletext = getArticleText(submission.url)
-    if articletext is not None:
-        if title.lower() in articletext.lower():
-            print('Submission has correct title. Ignored. Moving on...')
-        else:
-            print('Submission has wrong title.  Waiting 10 seconds for AutoModerator to check...')
-            time.sleep(10)  # Keeps Title Check Bot from removing submissions that AutoMod would have removed anyway.
-            submission.add_comment("Removed. Please see the sidebar for our rules.").distinguish()
-            submission.remove()
-            print('Submission removed. Moving on...')
+def titleCheck():
+    for submission in subreddit.get_unmoderated(limit=thing_limit):
+        title = submission.title
+        articletext = getArticleText(submission.url)
+        if articletext is not None:
+            if title.lower() in articletext.lower():
+                print('Submission has correct title. Ignored. Moving on...')
+            elif submission.score > 50:
+                print('Submission has wrong title, but has more than 50 upvotes. Reporting...')
+                submission.report(reason='Submission may have wrong title. Please review.')
+                print('Reported. Moving on...')
+            else:
+                print('Submission has wrong title.  Waiting 10 seconds for AutoModerator to check...')
+                time.sleep(10)  # Keeps Title Check Bot from removing submissions that AutoMod would have removed anyway.
+                submission.add_comment("Removed. Please see the sidebar for our rules.").distinguish()
+                submission.remove()
+                print('Removed submission by: "', submission.author.name, '". Moving on...')
+while True:
+    titleCheck()
+    print('Checked all unmoderated items. Sleeping for 30 seconds...')
+    time.sleep(30)
